dce49935e2a84c795d8de37e99a031969fb20463
David Lim <nicholai.david@gmail.com>
2017-11-10 21:30:35 +0800

    Initial commit

---
+++ PrismPlayground/PrismPlayground/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PrismPlayground.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
